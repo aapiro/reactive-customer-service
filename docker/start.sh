18eec07592ce7454af6a3cd7cfe09d0dd84fa24a
dserradji <dserradji@outlook.com>
2017-08-23 23:08:57 -0400

    start.sh and stop.sh can be called from anywhere

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd ..
+CALLER_DIRECTORY=$(pwd)
+SCRIPT_DIRECTORY=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+cd $SCRIPT_DIRECTORY/..
 mvn -Pdocker clean package
-cd docker
+
+cd $SCRIPT_DIRECTORY
 docker-compose up --build -d
+
+cd $CALLER_DIRECTORY
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+CALLER_DIRECTORY=$(pwd)
+SCRIPT_DIRECTORY=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+cd $SCRIPT_DIRECTORY
 docker-compose down
+
+cd $CALLER_DIRECTORY
